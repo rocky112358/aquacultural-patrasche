f818da88236ce4b0fcfc8cf8f2b541de29712051
Dongmin Kim <rocky112358@gmail.com>
2019-12-10 21:42:47 +0900

    ADD: implement vs command

--- src/app.py
+++ src/app.py
@@ -59,6 +59,16 @@ def up(update, context):
                 f"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n{_get_random_bark()}"
     context.bot.send_message(chat_id=update.effective_chat.id, text=resp_text,
                              reply_to_message_id=update.message.message_id)
+
+
+def vs(update, context):
+    if len(context.args) == 0:
+        return  # no answer for trolls
+    vs_string = " ".join(context.args)
+    vs_list = list(map(lambda x: x.strip(), vs_string.split("/")))
+    if any([len(x) == 0 for x in vs_list]):
+        return  # no answer for trolls
+    resp_text = random.sample(vs_list, 1)[0]
     context.bot.send_message(chat_id=update.effective_chat.id, text=resp_text,
                              reply_to_message_id=update.message.message_id)
 
@@ -68,11 +78,13 @@ start_handler = CommandHandler('start', start)
 roll_handler = CommandHandler('roll', roll)
 bark_handler = CommandHandler('bark', bark)
 up_handler = CommandHandler('up', up)
+vs_handler = CommandHandler('vs', vs)
 
 # dispatchers
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(roll_handler)
 dispatcher.add_handler(bark_handler)
 dispatcher.add_handler(up_handler)
+dispatcher.add_handler(vs_handler)
 
 updater.start_polling()
