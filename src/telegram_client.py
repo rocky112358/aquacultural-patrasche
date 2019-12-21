150a1beeec82ce9e5e90bdb5d3179ca9c17f9c1a
Dongmin Kim <rocky112358@gmail.com>
2019-12-22 00:26:13 +0900

    DEL: remove get_telegram_client

--- src/telegram_client.py
+++ src/telegram_client.py
@@ -7,19 +7,15 @@ API_ID = os.getenv('TELEGRAM_CORE_API_ID')
 API_HASH = os.getenv('TELEGRAM_CORE_API_HASH')
 PATRASCHE_ROOTDIR = os.getenv('PATRASCHE_ROOTDIR')
 
-def get_telegram_client():
-    telegram_client = TelegramClient(f"{PATRASCHE_ROOTDIR}anon", API_ID, API_HASH)
-    return telegram_client
+telegram_client = TelegramClient(f"{PATRASCHE_ROOTDIR}anon", API_ID, API_HASH)
 
 
 async def _get_online_users(chat_id):
-    telegram_client = get_telegram_client()
     users = await telegram_client.get_participants(chat_id)
     return users
 
 
 def get_online_users(chat_id):
-    telegram_client = get_telegram_client()
     with telegram_client:
         users = telegram_client.loop.run_until_complete(_get_online_users(chat_id))
         return_list = list()
@@ -30,7 +26,6 @@ def get_online_users(chat_id):
 
 
 async def _set_user_rank(chat_id, user_id, rank_text):
-    telegram_client = get_telegram_client()
     res = await telegram_client.edit_admin(chat_id, user_id, delete_messages=False, ban_users=False, invite_users=False,
                                            add_admins=False, is_admin=True, title=rank_text)
 
@@ -38,7 +33,6 @@ async def _set_user_rank(chat_id, user_id, rank_text):
 
 
 def set_user_rank(chat_id, user_id, rank_text):
-    telegram_client = get_telegram_client()
     with telegram_client:
         res = telegram_client.loop.run_until_complete(_set_user_rank(chat_id, user_id, rank_text))
         return res
