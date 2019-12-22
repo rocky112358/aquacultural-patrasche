dbeaa6a88a527c874ff074b317bcdce67dba992a
Dongmin Kim <rocky112358@gmail.com>
2019-12-22 18:29:01 +0900

    ADD: implement automatic self_bark

--- src/script_notice_balance.py
+++ src/script_notice_balance.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import telegram
 
@@ -7,10 +8,13 @@ from game import PatrascheCoin
 TELEGRAM_API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
 
 bot = telegram.Bot(TELEGRAM_API_TOKEN)
+BARK_INTERVAL = [60, 40, 20, 10, 5, 3]  # 5, 6, 7, 8, 9, 10
 
 if __name__ == '__main__':
     patrasche_coin = PatrascheCoin()
     bark_patrasche = False
+    interval = 0
+    total_bark = 0
 
     users = patrasche_coin.get_all_info()
 
@@ -19,8 +23,13 @@ if __name__ == '__main__':
         message_text += f"{user.name}(rank: {user.meow_count}): {user.balance}\n"
         if user.id == "patrasche" and user.balance >= 5 * 126000:
             bark_patrasche = True
+            bark_interval_index = user.balance // 126000 - 5
+            interval = BARK_INTERVAL[bark_interval_index]
+            total_bark = 600 // interval
 
     bot.send_message(-1001265183135, message_text)
     if bark_patrasche:
-        bot.send_message(-1001265183135, "지금부터 좀 짖어보겠습니다.")
-        patrasche_coin.self_bark()
+        bot.send_message(-1001265183135, f"지금부터 ({interval})초 간격으로 ({total_bark})번 짖어보겠습니다.")
+        for _ in range(total_bark):
+            patrasche_coin.self_bark()
+            time.sleep(interval)
