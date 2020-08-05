44085b6e648852ea84921419218431432f3ff1ac
Dongmin Kim <rocky112358@gmail.com>
2020-08-05 22:40:54 +0900

    CHG: change parse_mode for sending telegram message

--- src/api/telegram.py
+++ src/api/telegram.py
@@ -8,7 +8,8 @@ BOT_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
 
 def send_message(text: str):
     data = {"chat_id": -1001265183135,
-            "text": text}
+            "text": text,
+            "parse_mode": "HTML"}
 
     resp = requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", data=data)
     #print(resp.json())
